bae61435568fea23745a4b27ee0c97f5282b3301
Paul-Henri <102408695+Up-la@users.noreply.github.com>
2022-05-31 17:17:21 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
   def create
     @user = User.new(user_params)
     @user.save
   end
 
+  private 
+  
   def user_params
     params.require(:user).permit(:username, :photo)
   end
