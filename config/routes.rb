91c4eed6f6e6742e8d0465825b586f8044f70235
raja pichon <raja.ohl@gmail.com>
2022-05-31 17:22:53 +0200

    user photo

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resource :users, only: [:show, :index, :new]
+  resources :users, only: [:show, :index] 
+  resources :games, only: [:show, :index] 
+
 end
