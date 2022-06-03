af73ff5e46862e7367d4b3555041dec7e0c1203f
PaulHenri Leblanc <paulhenri.leblanc@gmail.com>
2022-06-03 15:44:52 +0200

    frontUserPage WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
+
   def my_ended_games
     games.where(ongoing: false)
   end
